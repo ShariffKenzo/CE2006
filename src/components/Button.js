08899409b695e35f7bb6029449511f2cb6cd6040
ShariffKenzo <72705719+ShariffKenzo@users.noreply.github.com>
2021-10-19 17:25:55 +0800

    first commit

---
+++ src/components/Button.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Button = ({ color, text}) => {
+
+    
+
+    return (
+    
+ <button 
+ className='menubutton' 
+ style ={{color:color}} >{text}</button>
+    )
+}
+
+export default Button
