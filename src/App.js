b05ad9bb4787560f6e044629e31c4a4aecb6012f
ShariffKenzo <72705719+ShariffKenzo@users.noreply.github.com>
2021-10-20 01:17:03 +0800

    with google maps

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
-
-import './App.css';
-import Header from './components/Header';
-import Button from './components/Button';
-//import AddOnList from "./AddOnList";
-//import DebounceInput from "react-debounce-input";
-import { DropdownButton } from 'react-bootstrap';
-import { MenuItem } from 'react-bootstrap';
+import "./App.css";
+import Header from "./components/Header";
+import Button from "./components/Button";
+import Backgroundimg from "./components/Backgroundimg";
+import SimpleMap from "./components/googlemap";
+import { ReactDOM } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      
+    <div>
+      <Backgroundimg />
+      <Header className="App" />
 
-  
-
-
-
+      <SimpleMap className="Googlemaps" />
     </div>
-  
-  
-
-
-
-
-
   );
 }
 
---
+++ src/components/Backgroundimg.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./HDBbackgroundnew.jpg";
+import "../App.css";
+import Header from "./Header";
+
+const Backgroundimg = () => {
+  return (
+    <div>
+      <img src={logo} alt="Logo" />
+    </div>
+  );
+};
+
+export default Backgroundimg;
