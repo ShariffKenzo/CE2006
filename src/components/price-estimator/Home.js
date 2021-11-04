5eff33dd43705a2ff29ebb3944dae94031a63e90
Jordan Chong <nadroj135@gmail.com>
2021-11-04 23:21:38 +0800

    integrate backend

--- src/components/price-estimator/Home.js
+++ src/components/price-estimator/Home.js
@@ -8,158 +8,115 @@ import BlockInfo from "./BlockInfo";
 import Dropdown from "../global/Dropdown";
 import "./PriceEstimatorFilters.css";
 import TownDB from "./TownDB";
-// import FlatTypeDB from "./FlatTypeDB";
-
+import * as gov from "../../logic/govData";
 
 const Home = () => {
-  const [selectedTown, setSelectedTown] = useState("");
-  const [streetDB, setStreetDB] = useState([]);
-  const [selectedStreet, setSelectedStreet] = useState("");
-  const [blockDB, setBlockDB] = useState([]);
-  const [selectedBlock, setSelectedBlock] = useState("");
-  const [flatTypeDB, setFlatTypeDB] = useState([]);
-  const [selectedFlatType, setSelectedFlatType] = useState("")
-  const [isDone, setIsDone] = useState(0);
-  const [info, setInfo] = useState([]);
-  useEffect(() => {
-    var filters = JSON.stringify({ town: selectedTown.toUpperCase() });
-      fetch(
-          `https://data.gov.sg/api/action/datastore_search?resource_id=f1765b54-a209-4718-8d38-a39237f502b3&filters=${filters}`
-      )
-          .then((response) => response.json())
-          .then((json) => {
-              console.log(`https://data.gov.sg/api/action/datastore_search?resource_id=f1765b54-a209-4718-8d38-a39237f502b3&filters=${filters}`)
-              let streets = [];
-              json["result"]["records"].map((item) => {
-                  streets.push(item["street_name"]);
-              });
-              setStreetDB([...new Set(streets)].sort());
-          });
-  }, [selectedTown]);
+    const [selectedTown, setSelectedTown] = useState("");
+    const [streetDB, setStreetDB] = useState([]);
+    const [selectedStreet, setSelectedStreet] = useState("");
+    const [blockDB, setBlockDB] = useState([]);
+    const [selectedBlock, setSelectedBlock] = useState("");
+    const [flatTypeDB, setFlatTypeDB] = useState([]);
+    const [selectedFlatType, setSelectedFlatType] = useState("");
+    const [isDone, setIsDone] = useState(0);
+    const [info, setInfo] = useState([]);
 
-  useEffect(() => {
-    var filters = JSON.stringify({ street_name: selectedStreet });
-      fetch(
-          `https://data.gov.sg/api/action/datastore_search?resource_id=f1765b54-a209-4718-8d38-a39237f502b3&filters=${filters}`
-      )
-          .then((response) => response.json())
-          .then((json) => {
-              console.log(`https://data.gov.sg/api/action/datastore_search?resource_id=f1765b54-a209-4718-8d38-a39237f502b3&filters=${filters}`)
-              let blocks = [];
-              json["result"]["records"].map((item) => {
-                  blocks.push(item["block"]);
-              });
-              setBlockDB([...new Set(blocks)].sort());
-          });
-  }, [selectedStreet, selectedTown]);
-  
-  useEffect(() => {
-    var filters = JSON.stringify({ street_name: selectedStreet, block: selectedBlock });
-      fetch(
-          `https://data.gov.sg/api/action/datastore_search?resource_id=f1765b54-a209-4718-8d38-a39237f502b3&filters=${filters}`
-      )
-          .then((response) => response.json())
-          .then((json) => {
-              console.log(`https://data.gov.sg/api/action/datastore_search?resource_id=f1765b54-a209-4718-8d38-a39237f502b3&filters=${filters}`)
-              let flatTypes = [];
-              json["result"]["records"].map((item) => {
-                  flatTypes.push(item["flat_type"]);
-              });
-              
-              setFlatTypeDB([...new Set(flatTypes)].sort());
-          });
-  }, [selectedBlock, selectedTown, selectedStreet]);
+    useEffect(() => {
+        selectedTown && gov.getStreets(selectedTown).then(setStreetDB);
+    }, [selectedTown]);
 
-  useEffect(()=>{
-    var filters = JSON.stringify({ street_name: selectedStreet, block: selectedBlock, flat_type: selectedFlatType.toUpperCase() });
-      fetch(
-          `https://data.gov.sg/api/action/datastore_search?resource_id=f1765b54-a209-4718-8d38-a39237f502b3&filters=${filters}`
-      )
-          .then((response) => response.json())
-          .then((json) => {
-              console.log(`https://data.gov.sg/api/action/datastore_search?resource_id=f1765b54-a209-4718-8d38-a39237f502b3&filters=${filters}`)
-              console.log(json['result']['records'])
-              setInfo(json['result']['records'])
-          });
+    useEffect(() => {
+        selectedStreet && gov.getBlocks(selectedStreet).then(setBlockDB);
+    }, [selectedStreet]);
 
-  }, [isDone])
+    useEffect(() => {
+      selectedBlock && gov.getFlatType(selectedStreet, selectedBlock).then(setFlatTypeDB);
+    }, [selectedBlock]);
 
-  const townSelectHandler = (town) => {
-      setSelectedTown(town);
-      setSelectedStreet('')
-      setSelectedBlock('')
-      setSelectedFlatType('')
-  };
-  const streetSelectHandler = (street) => {
-      setSelectedStreet(street);
-      setSelectedBlock('')
-      setSelectedFlatType('')
-  };
-  const blockSelectHandler = (block) => {
-      setSelectedBlock(block);
-      setSelectedFlatType('')
-  };
-  const flatTypeSelectHandler = (flatType) => {
-    setSelectedFlatType(flatType);
-    setIsDone(isDone+1)
-};
+    useEffect(() => {
+        var filters = JSON.stringify({
+            street_name: selectedStreet,
+            block: selectedBlock,
+            flat_type: selectedFlatType,
+        });
+        isDone && gov.getMain("f1765b54-a209-4718-8d38-a39237f502b3", {}, filters, true).then(setInfo)
+    }, [isDone]);
 
-  return (
-    <div>
-      <frag>
-        <Backgroundimg />
-      </frag>
-      <frag>
-        <Header />
-      </frag>
+    const townSelectHandler = (town) => {
+        setSelectedTown(town);
+        setSelectedStreet("");
+        setSelectedBlock("");
+        setSelectedFlatType("");
+    };
+    const streetSelectHandler = (street) => {
+        setSelectedStreet(street);
+        setSelectedBlock("");
+        setSelectedFlatType("");
+    };
+    const blockSelectHandler = (block) => {
+        setSelectedBlock(block);
+        setSelectedFlatType("");
+    };
+    const flatTypeSelectHandler = (flatType) => {
+        setSelectedFlatType(flatType);
+        setIsDone(isDone + 1);
+    };
 
-      <frag>
-      <div>
-            <Dropdown
-                className="Townest"
-                label="Town"
-                options={TownDB}
-                value = {selectedTown}
-                onSelectOption={townSelectHandler}
-            />
-            <Dropdown
-                className="Streetest"
-                label="Street"
-                value = {selectedStreet}
-                options={streetDB}
-                onSelectOption={streetSelectHandler}
-            />
-            <Dropdown
-                className="Typreest"
-                label="Block"
-                value = {selectedBlock}
-                options={blockDB}
-                onSelectOption={blockSelectHandler}
-            />
-            <Dropdown
-                className="Blkest"
-                label="Flat Type"
-                value = {selectedFlatType}
-                options={flatTypeDB}
-                onSelectOption={flatTypeSelectHandler}
-            />
+    return (
+        <div>
+            <frag>
+                <Backgroundimg />
+            </frag>
+            <frag>
+                <Header />
+            </frag>
 
-        </div>
-      </frag>
+            <frag>
+                <div>
+                    <Dropdown
+                        className="Townest"
+                        label="Town"
+                        options={TownDB}
+                        value={selectedTown}
+                        onSelectOption={townSelectHandler}
+                    />
+                    <Dropdown
+                        className="Streetest"
+                        label="Street"
+                        value={selectedStreet}
+                        options={streetDB}
+                        onSelectOption={streetSelectHandler}
+                    />
+                    <Dropdown
+                        className="Typreest"
+                        label="Block"
+                        value={selectedBlock}
+                        options={blockDB}
+                        onSelectOption={blockSelectHandler}
+                    />
+                    <Dropdown
+                        className="Blkest"
+                        label="Flat Type"
+                        value={selectedFlatType}
+                        options={flatTypeDB}
+                        onSelectOption={flatTypeSelectHandler}
+                    />
+                </div>
+            </frag>
 
-      <frag>
-        <SimpleMap className="Googlemaps" />
-      </frag>
-      {/*<frag>
+            <frag>
+                <SimpleMap className="Googlemaps" street={selectedStreet} block={selectedBlock}/>
+            </frag>
+            {/*<frag>
         <BlockInfo info = {info}/>
       </frag>*/}
-      <div styles={{ height: '500px', overflowY: 'scroll' }} //style={styles.wrapperDiv}
-      >
-      <BlockInfo info = {info}/>
-</div>
-
-    </div>
-  );
+            <div
+                styles={{ height: "500px", overflowY: "scroll" }} //style={styles.wrapperDiv}
+            >
+                <BlockInfo info={info} />
+            </div>
+        </div>
+    );
 };
 
 export default Home;
--- src/components/price-estimator/PriceEstimatorMap.js
+++ src/components/price-estimator/PriceEstimatorMap.js
@@ -1,40 +1,47 @@
-import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
+import React, { Component } from "react";
+import GoogleMapReact from "google-map-react";
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 /**
  * Renders the Google maps component
  */
-class PriceEstimatorMap extends Component {
-  static defaultProps = {
-    center: {
-      lat: 1.3483,
-      lng: 103.6831
-    },
-    zoom: 11
-  };
+const PriceEstimatorMap = (props) => {
+    const defaultProps = {
+        center: {
+            lat: 1.3483,
+            lng: 103.6831,
+        },
+        zoom: 11,
+    };
+    const street = props.street
+    const block = props.block
 
-  render() {
     return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: '70vh',  width:'50%',  position: 'absolute', left: '28%', top: '60%',
-      transform: 'translate(-50%, -50%)' }}>
-        <GoogleMapReact
-            ////// uncomment bootstrap when inputing key "Backend"/////
-         // bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
+        // Important! Always set the container height explicitly
+        <div
+            style={{
+                height: "70vh",
+                width: "50%",
+                position: "absolute",
+                left: "28%",
+                top: "60%",
+                transform: "translate(-50%, -50%)",
+            }}
         >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text="My Marker"
-          />
-          
-        </GoogleMapReact>
-      </div>
+            <GoogleMapReact
+                ////// uncomment bootstrap when inputing key "Backend"/////
+                // bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
+                defaultCenter={this.props.center}
+                defaultZoom={this.props.zoom}
+            >
+                <AnyReactComponent
+                    lat={59.955413}
+                    lng={30.337844}
+                    text="My Marker"
+                />
+            </GoogleMapReact>
+        </div>
     );
-  }
-}
+};
 
 export default PriceEstimatorMap;
